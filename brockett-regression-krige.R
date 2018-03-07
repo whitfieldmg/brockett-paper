62da000de1f58b89d786a03ba55db5e5e5053656
whitfieldmg <mgwhitfield@gmail.com>
2018-03-07 13:13:23 +0000

    Added models and outputs for fourth depth

--- brockett-regression-krige.R
+++ brockett-regression-krige.R
@@ -6,6 +6,7 @@ library(tidyr)
 library(here)
 library(automap)
 library(readr)
+library(lattice)
 
 ## Function from Stackoverflow to remove rows containing missing data from spatial objects
 # FUNCTION TO REMOVE NA's IN sp DataFrame OBJECT
@@ -44,6 +45,21 @@ extract_krige_stats <- function(krige_model) {
   return(stat_table)
 }
 
+# vgmaplot - adapted plot.autofitVariogram function to allow custom title
+vgmaplot = function(x, title = title, plotit = TRUE, ...)
+{ 
+  shift = 0.03
+  labels = as.character(x$exp_var$np)
+  vario = xyplot(gamma ~ dist, data = x$exp_var, panel = automap:::autokrige.vgm.panel,
+                 labels = labels, shift = shift, model = x$var_model,# subscripts = TRUE,
+                 direction = c(x$exp_var$dir.hor[1], x$exp_var$dir.ver[1]),
+                 ylim = c(min(0, 1.04 * min(x$exp_var$gamma)), 1.04 * max(x$exp_var$gamma)),
+                 xlim = c(0, 1.04 * max(x$exp_var$dist)), xlab = "Distance", ylab = "Semi-variance",
+                 main = title, mode = "direct",...)
+  if (plotit) print(vario) else vario
+}
+
+
 # Set path to project
 (projdir <- paste0(here(), "/brockett-soilc/"))
 
@@ -62,12 +78,15 @@ lowsnab.gdb <- paste0(projdir, "MikeLowsnab.gdb")
 birkhowe_d1 <- readOGR(dsn = birkhowe.gdb, layer = "birkhowe_soild1_pt_v5", pointDropZ = TRUE)
 birkhowe_d2 <- readOGR(dsn = birkhowe.gdb, layer = "birkhowe_soild2_pt_v5", pointDropZ = TRUE)
 birkhowe_d3 <- readOGR(dsn = birkhowe.gdb, layer = "birkhowe_soild3_pt_v5", pointDropZ = TRUE)
+birkhowe_d4 <- readOGR(dsn = birkhowe.gdb, layer = "birkhowe_soild4_pt_v5", pointDropZ = TRUE)
 hollins_d1 <- readOGR(dsn = hollins.gdb, layer = "hollins_soild1_pt_v3", pointDropZ = TRUE)
 hollins_d2 <- readOGR(dsn = hollins.gdb, layer = "hollins_soild2_pt_v3", pointDropZ = TRUE)
 hollins_d3 <- readOGR(dsn = hollins.gdb, layer = "hollins_soild3_pt_v3", pointDropZ = TRUE)
+hollins_d4 <- readOGR(dsn = hollins.gdb, layer = "hollins_soild4_pt_v3", pointDropZ = TRUE)
 lowsnab_d1 <- readOGR(dsn = lowsnab.gdb, layer = "lowsnab_soild1_pt_v5", pointDropZ = TRUE)
 lowsnab_d2 <- readOGR(dsn = lowsnab.gdb, layer = "lowsnab_soild2_pt_v5", pointDropZ = TRUE)
 lowsnab_d3 <- readOGR(dsn = lowsnab.gdb, layer = "lowsnab_soild3_pt_v5", pointDropZ = TRUE)
+lowsnab_d4 <- readOGR(dsn = lowsnab.gdb, layer = "lowsnab_soild4_pt_v5", pointDropZ = TRUE)
 
 # Read elevation data for Hollins (missing from geodatabase)
 hollins_elevation <- read_csv(paste0(projdir, "hollins_elevation.csv"))
@@ -85,32 +104,45 @@ hollins_d2@data <- select(hollins_d2@data, -elevation, -hls_plot) %>%
 hollins_d3@data <- select(hollins_d3@data, -elevation, -hls_plot) %>%
                     rename(hls_plot = hls_vegcomm) %>%
                     left_join(hollins_elevation, by = "gps_id")
+hollins_d4@data <- select(hollins_d4@data, -elevation, -hls_plot) %>%
+                    rename(hls_plot = hls_vegcomm) %>%
+                    left_join(hollins_elevation, by = "gps_id")
+
 
 # Subset necessary columns from data
 sub_cols <- c("hls_plot", "elevation", "moisture", "totC_mean_mass_vol")
 birkhowe_d1_sub <- birkhowe_d1[, sub_cols]
 birkhowe_d2_sub <- birkhowe_d2[, sub_cols]
 birkhowe_d3_sub <- birkhowe_d3[, sub_cols]
+birkhowe_d4_sub <- birkhowe_d4[, sub_cols]
 
 hollins_d1_sub <- hollins_d1[, sub_cols]
 hollins_d2_sub <- hollins_d2[, sub_cols]
 hollins_d3_sub <- hollins_d3[, sub_cols]
+hollins_d4_sub <- hollins_d4[, sub_cols]
 
 lowsnab_d1_sub <- lowsnab_d1[, sub_cols]
 lowsnab_d2_sub <- lowsnab_d2[, sub_cols]
 lowsnab_d3_sub <- lowsnab_d3[, sub_cols]
+lowsnab_d4_sub <- lowsnab_d4[, sub_cols]
 
 # Filter rows containing NAs and remove duplicates
 # Overwrite _sub object
 birkhowe_d1_sub <- sp.na.omit(remove.duplicates(birkhowe_d1_sub))
 birkhowe_d2_sub <- sp.na.omit(remove.duplicates(birkhowe_d2_sub))
 birkhowe_d3_sub <- sp.na.omit(remove.duplicates(birkhowe_d3_sub))
+birkhowe_d4_sub <- sp.na.omit(remove.duplicates(birkhowe_d4_sub))
+
 hollins_d1_sub <- sp.na.omit(remove.duplicates(hollins_d1_sub))
 hollins_d2_sub <- sp.na.omit(remove.duplicates(hollins_d2_sub))
 hollins_d3_sub <- sp.na.omit(remove.duplicates(hollins_d3_sub))
+hollins_d4_sub <- sp.na.omit(remove.duplicates(hollins_d4_sub))
+
 # lowsnab_d1_sub <- sp.na.omit(lowsnab_d1_sub) # No empty cells. Function returns empty data
 lowsnab_d2_sub <- sp.na.omit(remove.duplicates(lowsnab_d2_sub))
 lowsnab_d3_sub <- sp.na.omit(remove.duplicates(lowsnab_d3_sub))
+lowsnab_d4_sub <- sp.na.omit(remove.duplicates(lowsnab_d4_sub))
+
 
 # Interactive variogram fitting procedure
 # Run these lines once
@@ -238,6 +270,11 @@ birkhowe_d3.vgmfit <- fit.variogram(variogram(totC_mean_mass_vol ~ 1, birkhowe_d
                                     fit.kappa = TRUE,
                                     debug.level = 1)
 
+birkhowe_d4.vgmfit <- fit.variogram(variogram(totC_mean_mass_vol ~ 1, birkhowe_d4_sub),
+                                    vgm(c("Exp", "Mat", "Sph", "Ste", "Gau")),
+                                    fit.kappa = TRUE,
+                                    debug.level = 1)
+
 lowsnab_d1.vgmfit <- fit.variogram(variogram(totC_mean_mass_vol ~ 1, lowsnab_d1_sub),
                                     vgm(c("Exp", "Mat", "Sph", "Ste", "Gau")),
                                     fit.kappa = TRUE,
@@ -249,6 +286,11 @@ lowsnab_d2.vgmfit <- fit.variogram(variogram(totC_mean_mass_vol ~ 1, lowsnab_d2_
                                    debug.level = 1)
 
 lowsnab_d3.vgmfit <- fit.variogram(variogram(totC_mean_mass_vol ~ 1, lowsnab_d3_sub),
+                                   vgm(c("Exp", "Mat", "Sph", "Ste", "Gau")),
+                                   fit.kappa = TRUE,
+                                   debug.level = 1)
+
+lowsnab_d4.vgmfit <- fit.variogram(variogram(totC_mean_mass_vol ~ 1, lowsnab_d4_sub),
                                    vgm(c("Exp", "Mat", "Sph", "Ste", "Gau")),
                                    fit.kappa = TRUE,
                                    debug.level = 1)
@@ -268,6 +310,11 @@ hollins_d3.vgmfit <- fit.variogram(variogram(totC_mean_mass_vol ~ 1, hollins_d3_
                                    fit.kappa = TRUE,
                                    debug.level = 1)
 
+hollins_d4.vgmfit <- fit.variogram(variogram(totC_mean_mass_vol ~ 1, hollins_d4_sub),
+                                   vgm(c("Exp", "Mat", "Sph", "Ste", "Gau")),
+                                   fit.kappa = TRUE,
+                                   debug.level = 1)
+
 # Investigate utility of automap::autofit
 
 birkhowe_d1.vgmafit <- autofitVariogram(totC_mean_mass_vol ~ 1,
@@ -282,6 +329,10 @@ birkhowe_d3.vgmafit <- autofitVariogram(totC_mean_mass_vol ~ 1,
                                         birkhowe_d3_sub,
                                         verbose = TRUE)
 
+birkhowe_d4.vgmafit <- autofitVariogram(totC_mean_mass_vol ~ 1,
+                                        birkhowe_d4_sub,
+                                        verbose = TRUE)
+
 lowsnab_d1.vgmafit <- autofitVariogram(totC_mean_mass_vol ~1,
                                        lowsnab_d1_sub,
                                        verbose = TRUE)
@@ -294,6 +345,10 @@ lowsnab_d3.vgmafit <- autofitVariogram(totC_mean_mass_vol ~ 1,
                                        lowsnab_d3_sub,
                                        verbose = TRUE)
 
+lowsnab_d4.vgmafit <- autofitVariogram(totC_mean_mass_vol ~ 1,
+                                       lowsnab_d4_sub,
+                                       verbose = TRUE)
+
 hollins_d1.vgmafit <- autofitVariogram(totC_mean_mass_vol ~ 1,
                                        hollins_d1_sub,
                                        verbose = TRUE)
@@ -304,6 +359,10 @@ hollins_d2.vgmafit <- autofitVariogram(totC_mean_mass_vol ~ 1,
 
 hollins_d3.vgmafit <- autofitVariogram(totC_mean_mass_vol ~ 1,
                                        hollins_d3_sub,
+                                       verbose = TRUE)
+
+hollins_d4.vgmafit <- autofitVariogram(totC_mean_mass_vol ~ 1,
+                                       hollins_d4_sub,
                                        verbose = TRUE)
 
 
@@ -355,6 +414,21 @@ birkhowe_d3.afcv <- krige.cv(formula = totC_mean_mass_vol ~ elevation + moisture
 
 (birkhowe_d3.astats <- extract_krige_stats(birkhowe_d3.afcv))
 
+# Birkhowe d4
+birkhowe_d4.cv <- krige.cv(formula = totC_mean_mass_vol ~ elevation + moisture + hls_plot,
+                           birkhowe_d4_sub,
+                           model = birkhowe_d4.vgmfit,
+                           debug.level = 2)
+
+(birkhowe_d4.stats <- extract_krige_stats(birkhowe_d4.cv))
+
+birkhowe_d4.afcv <- krige.cv(formula = totC_mean_mass_vol ~ elevation + moisture + hls_plot,
+                             birkhowe_d4_sub,
+                             model = birkhowe_d4.vgmafit$var_model,
+                             debug.level = 2)
+
+(birkhowe_d4.astats <- extract_krige_stats(birkhowe_d4.afcv))
+
 # Lowsnab d1
 lowsnab_d1.cv <- krige.cv(formula = totC_mean_mass_vol ~ elevation + moisture + hls_plot,
                           lowsnab_d1_sub,
@@ -399,6 +473,21 @@ lowsnab_d3.afcv <- krige.cv(formula = totC_mean_mass_vol ~ elevation + moisture 
                           debug.level = 2)
 
 (lowsnab_d3.astats <- extract_krige_stats(lowsnab_d3.afcv))
+
+# Lowsnab D4
+lowsnab_d4.cv <- krige.cv(formula = totC_mean_mass_vol ~ elevation + moisture + hls_plot,
+                          lowsnab_d4_sub,
+                          model = lowsnab_d4.vgmfit,
+                          debug.level = 2)
+
+(lowsnab_d4.stats <- extract_krige_stats(lowsnab_d4.cv))
+
+lowsnab_d4.afcv <- krige.cv(formula = totC_mean_mass_vol ~ elevation + moisture + hls_plot,
+                            lowsnab_d4_sub,
+                            model = lowsnab_d4.vgmafit$var_model,
+                            debug.level = 2)
+
+(lowsnab_d4.astats <- extract_krige_stats(lowsnab_d4.afcv))
 
 # Hollins D1
 hollins_d1.cv <- krige.cv(formula = totC_mean_mass_vol ~ elevation + moisture + hls_plot,
@@ -445,37 +534,58 @@ hollins_d3.afcv <- krige.cv(formula = totC_mean_mass_vol ~ elevation + moisture 
 
 (hollins_d3.astats <- extract_krige_stats(hollins_d3.afcv))
 
+# Hollins D4
+hollins_d4.cv <- krige.cv(formula = totC_mean_mass_vol ~ elevation + moisture + hls_plot,
+                          hollins_d4_sub,
+                          model = hollins_d4.vgmfit,
+                          debug.level = 2)
+
+(hollins_d4.stats <- extract_krige_stats(hollins_d4.cv))
+
+hollins_d4.afcv <- krige.cv(formula = totC_mean_mass_vol ~ elevation + moisture + hls_plot,
+                            hollins_d4_sub,
+                            model = hollins_d4.vgmafit$var_model,
+                            debug.level = 2)
+
+(hollins_d4.astats <- extract_krige_stats(hollins_d4.afcv))
+
 
 # Consolidate kriging summaries into site tables
 
 birkhowe.stats <- bind_rows(list(birkhowe_d1.stats,
                                  birkhowe_d2.stats,
-                                 birkhowe_d3.stats),
+                                 birkhowe_d3.stats,
+                                 birkhowe_d4.stats),
                             .id = "depth")
 
 lowsnab.stats <- bind_rows(list(lowsnab_d1.stats,
                                 lowsnab_d2.stats,
-                                lowsnab_d3.stats),
+                                lowsnab_d3.stats,
+                                lowsnab_d4.stats),
                            .id = "depth")
 
 hollins.stats <- bind_rows(list(hollins_d1.stats,
                                 hollins_d2.stats,
-                                hollins_d3.stats),
+                                hollins_d3.stats,
+                                hollins_d4.stats),
                            .id = "depth")
 
 birkhowe.astats <- bind_rows(list(birkhowe_d1.astats,
                                  birkhowe_d2.astats,
-                                 birkhowe_d3.astats),
+                                 birkhowe_d3.astats,
+                                 birkhowe_d4.astats),
                             .id = "depth")
 
 lowsnab.astats <- bind_rows(list(lowsnab_d1.astats,
                                 lowsnab_d2.astats,
-                                lowsnab_d3.astats),
+                                lowsnab_d3.astats,
+                                lowsnab_d4.astats),
                            .id = "depth")
 
 hollins.astats <- bind_rows(list(hollins_d1.astats,
                                 hollins_d2.astats,
-                                hollins_d3.astats),
+                                hollins_d3.astats,
+                                hollins_d4.astats),
                            .id = "depth")
 
 # A table of tables for export
@@ -483,7 +593,6 @@ allsite.stats <- bind_rows(list(birkhowe = birkhowe.stats,
                                 lowsnab = lowsnab.stats,
                                 hollins = hollins.stats),
                            .id = "site") 
-                  
 
 allsite.stats
 
@@ -502,42 +611,113 @@ write.csv(allsite.astats, file = paste0(projdir, "allsite_astats.csv"),
 
 birkhowe.cv.results <- bind_rows(list(as.data.frame(birkhowe_d1.cv),
                                        as.data.frame(birkhowe_d2.cv),
-                                       as.data.frame(birkhowe_d3.cv)),
+                                       as.data.frame(birkhowe_d3.cv),
+                                       as.data.frame(birkhowe_d4.cv)),
                         .id = "Depth")
 
 write_csv(birkhowe.cv.results, paste0(projdir, "birkhowe_cv_results.csv"))
 
 birkhowe.acv.results <- bind_rows(list(as.data.frame(birkhowe_d1.afcv),
                                        as.data.frame(birkhowe_d2.afcv),
-                                       as.data.frame(birkhowe_d3.afcv)),
+                                       as.data.frame(birkhowe_d3.afcv),
+                                       as.data.frame(birkhowe_d4.afcv)),
                                   .id = "Depth")
 
 write_csv(birkhowe.acv.results, paste0(projdir, "birkhowe_acv_results.csv"))
 
 lowsnab.cv.results <- bind_rows(list(as.data.frame(lowsnab_d1.cv),
                                       as.data.frame(lowsnab_d2.cv),
-                                      as.data.frame(lowsnab_d3.cv)),
+                                      as.data.frame(lowsnab_d3.cv),
+                                      as.data.frame(lowsnab_d4.cv)),
                                  .id = "Depth")
 
 write_csv(lowsnab.cv.results, paste0(projdir, "lowsnab_cv_results.csv"))
 
 lowsnab.acv.results <- bind_rows(list(as.data.frame(lowsnab_d1.afcv),
                                        as.data.frame(lowsnab_d2.afcv),
-                                       as.data.frame(lowsnab_d3.afcv)),
+                                       as.data.frame(lowsnab_d3.afcv),
+                                       as.data.frame(lowsnab_d4.afcv)),
                                   .id = "Depth")
 
 write_csv(lowsnab.acv.results, paste0(projdir, "lowsnab_acv_results.csv"))
 
 hollins.cv.results <- bind_rows(list(as.data.frame(hollins_d1.cv),
                                       as.data.frame(hollins_d2.cv),
-                                      as.data.frame(hollins_d3.cv)),
+                                      as.data.frame(hollins_d3.cv),
+                                      as.data.frame(hollins_d4.cv)),
                                  .id = "Depth")
 
 write_csv(hollins.cv.results, paste0(projdir, "hollins_cv_results.csv"))
 
 hollins.acv.results <- bind_rows(list(as.data.frame(hollins_d1.afcv),
                                        as.data.frame(hollins_d2.afcv),
-                                       as.data.frame(hollins_d3.afcv)),
+                                       as.data.frame(hollins_d3.afcv),
+                                       as.data.frame(hollins_d4.afcv)),
                                   .id = "Depth")
 
 write_csv(hollins.acv.results, paste0(projdir, "hollins_acv_results.csv"))
+
+## Plot variograms (automap)
+
+# Birkhowe
+png(filename = paste0(projdir, "birkhowe-d1.png"),
+    width = 600, height = 380)
+vgmaplot(birkhowe_d1.vgmafit, title = "Birkhowe, 0-20 cm")
+dev.off()
+
+png(filename = paste0(projdir, "birkhowe-d2.png"),
+    width = 600, height = 380)
+vgmaplot(birkhowe_d2.vgmafit, title = "Birkhowe, 20-40 cm")
+dev.off()
+
+png(filename = paste0(projdir, "birkhowe-d3.png"),
+    width = 600, height = 380)
+vgmaplot(birkhowe_d3.vgmafit, title = "Birkhowe, 40-60 cm")
+dev.off()
+
+png(filename = paste0(projdir, "birkhowe-d4.png"),
+    width = 600, height = 380)
+vgmaplot(birkhowe_d4.vgmafit, title = "Birkhowe, 60-80 cm")
+dev.off()
+
+# Hollins
+png(filename = paste0(projdir, "hollins-d1.png"),
+    width = 600, height = 380)
+vgmaplot(hollins_d1.vgmafit, title = "Hollins, 0-20 cm")
+dev.off()
+
+png(filename = paste0(projdir, "hollins-d2.png"),
+    width = 600, height = 380)
+vgmaplot(hollins_d2.vgmafit, title = "Hollins, 20-40 cm")
+dev.off()
+
+png(filename = paste0(projdir, "hollins-d3.png"),
+    width = 600, height = 380)
+vgmaplot(hollins_d3.vgmafit, title = "Hollins, 40-60 cm")
+dev.off()
+
+png(filename = paste0(projdir, "hollins-d4.png"),
+    width = 600, height = 380)
+vgmaplot(hollins_d4.vgmafit, title = "Hollins, 60-80 cm")
+dev.off()
+
+# Lowsnab
+png(filename = paste0(projdir, "lowsnab-d1.png"),
+    width = 600, height = 380)
+vgmaplot(lowsnab_d1.vgmafit, title = "Lowsnab, 0-20 cm")
+dev.off()
+
+png(filename = paste0(projdir, "lowsnab-d2.png"),
+    width = 600, height = 380)
+vgmaplot(lowsnab_d2.vgmafit, title = "Lowsnab, 20-40 cm")
+dev.off()
+
+png(filename = paste0(projdir, "lowsnab-d3.png"),
+    width = 600, height = 380)
+vgmaplot(lowsnab_d3.vgmafit, title = "Lowsnab, 40-60 cm")
+dev.off()
+
+png(filename = paste0(projdir, "lowsnab-d4.png"),
+    width = 600, height = 380)
+vgmaplot(lowsnab_d3.vgmafit, title = "Lowsnab, 60-80 cm")
+dev.off()
